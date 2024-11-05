caa34bb89911da7275e973575ef58e50c6c427d2
tomforts <tomfortin2004@gmail.com>
2024-11-05 13:16:03 -0500

    create post
    asas

---
+++ models/post.js
@@ -0,0 +1,15 @@
+import Model from './model.js';
+
+export default class Bookmark extends Model {
+    constructor() {
+        super(true /* secured Id */);
+
+        this.addField('Title', 'string');
+        this.addField('Text', 'string');
+        this.addField('Category', 'string');
+        this.addField('Image', 'asset');
+        this.addField('Creation', 'integer');
+              
+        this.setKey("Title");
+    }
+}
